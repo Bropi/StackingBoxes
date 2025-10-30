e67e94e3a2809f0f8e299c768ea1dff11c2f4724
Bropi <boerishhorstink@gmail.com>
2025-10-30 17:47:02 +0100

    scene switcher
    very simple scene switcher added

---
+++ src/autoloads/scene_switcher.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var current_scene: Node = null
+
+func _ready():
+	current_scene = get_tree().current_scene
+
+func switch_to_scene(path):
+	get_tree().change_scene_to_file(path)
--- src/main/mainV2.gd
+++ src/main/mainV2.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
+func _ready() -> void:
+	start_game()
 
 #this function is triggered by a signal in mainV2.gd
 func _on_level_reload_scene() -> void:
 	get_tree().reload_current_scene()
+
+func start_game():
+	#switches to level scene
+	SceneSwitcher.switch_to_scene("res://src/levels/level.tscn")
